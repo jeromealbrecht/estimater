12adc70bbac3c83be44a826275d59032db12a256
December81 <megastudio@outlook.fr>
2024-08-12 14:35:15 +0200

    add first base component estimator

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { EstimatorComponent } from './estimator/estimator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'estimator', component: EstimatorComponent },
+    // autres routes
+  ];
---
+++ src/app/estimator/estimator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estimator',
+  standalone: true,
+  imports: [],
+  templateUrl: './estimator.component.html',
+  styleUrl: './estimator.component.css'
+})
+export class EstimatorComponent {
+
+}
